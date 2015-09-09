f2a9fccef077119306dd6b043e8d894441ea1045
Samee-Swartz <Samee.swartz@gmail.com>
2015-09-09 13:07:37 -0400

    created TurnNode

--- Project1/AbstractNode.h
+++ Project1/AbstractNode.h
@@ -1,10 +1,11 @@
 
+// Abstract Class representing a Node.
 class AbstractNode {
 public:
-	AbstractNode(RobotState s, float p) : prevCost(p), curState(s) {
+	AbstractNode(RobotState s, float p) : prevCost(p) {
 		heuristic = World.getInstance().getHeuristic();
 		offGrid = false;
-		// Extended classes need to calculate travelCost and totalCost
+		// Extended classes need to calculate curSTate, travelCost, and totalCost
 	}
 	void onEnter(void) {};
 	void onExit(void) {};
@@ -12,10 +13,16 @@ public:
 	float getTotalCost() const { return totalCost; }
 
 private:
+	// determines whether this node moves the robot off the world
 	bool offGrid;	// Calculated in constructor. If world terrain >= -100
+	// heuristic to the goal
 	float heuristic;
+	// travel cost for this node on this terrain
 	float travelCost;
+	// previous cost to get to this node
 	float prevCost;
+	// total = heuristic + travel + previous
 	float totalCost;
+	// the state of the robot on this node
 	RobotState curState;
 };
