e0cbd7479ab3b492aeca5a16fc479a0787b3c082
Benny Peake <bennypeake@gmail.com>
2015-09-10 14:52:32 -0400

    Added members to AbstractNode

--- Project1/AbstractNode.h
+++ Project1/AbstractNode.h
@@ -13,13 +13,19 @@ public:
 	void onEnter(void) {};
 	void onExit(void) {};
 	virtual void spawnChildren(void) = 0;
-	int getTotalCost() const { return totalCost; }
+	int getTotalCost() const { return totalCost; }''
 
-private:
+	bool isGoal() {return isGoal;};
+	bool isOffGrid(){return offGrid;};
+	bool isEnd(){return isGoal || offGrid;}
+
+protected:
 	// determines whether this node moves the robot off the world
 	bool offGrid;	// Calculated in constructor. If world terrain >= -100
 	// heuristic to the goal
 	int heuristic;
+	// does this node reach a goal state
+	bool isGoal;
 	// travel cost for this node on this terrain
 	int travelCost;
 	// previous cost to get to this nodefloat
